ae172254b2eda7cd9ce0f3f74cb9f6b348cdc20c
Andrew Romanovsky <rmansorokin@gmail.com>
2018-08-07 10:35:22 +0300

    refactored main

--- app/Main.hs
+++ app/Main.hs
@@ -10,36 +10,7 @@ import Network.HTTP.Types (status200, methodGet, methodPost, Query)
 import Network.Wai.Handler.Warp (run)
 
 --type Application = Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived
-{-application request respond = do
-  print $ pathInfo request
-  print $ queryString request
-  respond $
-    responseLBS status200 [("Content-Type", "text/plain")] "Hello World"-}
-application request respond = do
-  c <- connect defaultConnectInfo {
-      connectDatabase = "news-server"
-    , connectUser = "news-server"
-    , connectPassword = "news-server" 
-  }
-  let method = requestMethod request
-      path = pathInfo request
-      qString = queryString request
-  if method == methodGet
-     then if null qString || fst (head qString) == "page"
-             || fst (head qString) == "sort_by"
-             then processGetRequest request c >>= respond
-             else processFilterGetRequest request c >>= respond
-     else
-      processPostRequest request c >>= respond
-
-{-
-  let mPage = lookup "page" $ queryString request
-      page = mPage >>=
-        maybe Nothing (decode . B.fromStrict:: BS.ByteString -> Maybe Integer)
--}
-
-                     {-query = queryString request
-                     in processAppRequest path query >>= respond
-            else processPostRequest request >>= respond-}
+application :: Application
+application request respond = processRequest request >>= respond
 
 main = run 3000 application
--- src/Requests.hs
+++ src/Requests.hs
@@ -1,4 +1,32 @@
-module Requests (processPostRequest, processGetRequest, processFilterGetRequest) where
+{-# LANGUAGE OverloadedStrings #-}
+module Requests (processRequest) where
 
 import PostRequests
 import GetRequests
+import User
+import Network.Wai
+import Database.PostgreSQL.Simple hiding (Query)
+import Network.HTTP.Types (status200, methodGet, methodPost, Query)
+import Network.Wai.Handler.Warp (run)
+
+processRequest :: Request -> IO Response
+processRequest request = do
+  c <- connect defaultConnectInfo {
+      connectDatabase = "news-server"
+    , connectUser = "news-server"
+    , connectPassword = "news-server" 
+  }
+  let method = requestMethod request
+      qString = queryString request
+  if method == methodGet
+     then if isSimpleGet qString
+             then processGetRequest request c
+             else processFilterGetRequest request c
+     else
+      processPostRequest request c
+
+isSimpleGet :: Query -> Bool
+isSimpleGet [] = True
+isSimpleGet (("page", p) : qs) = True
+isSimpleGet (("sort_by", sB) : qs) = True
+isSimpleGet _ = False
